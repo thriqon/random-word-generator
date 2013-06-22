aa7a43ab46a6fc0e7a3285ad1b451cbfc6baa1ec
Jonas Weber <mail@jonasw.de>
2013-06-22 03:06:37 +0200

    current api-documentation

---
+++ apidocs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Generator"
+    ],
+    "modules": [
+        "Generator"
+    ],
+    "allModules": [
+        {
+            "displayName": "Generator",
+            "name": "Generator",
+            "description": "The module providing the generator"
+        }
+    ]
+} };
+});
